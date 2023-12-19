8a53c8a0b301542d94cae600f2591e839108c2b6
Nonso Anetoh <anet0001@algonquinlive.com>
2023-12-19 12:30:56 -0500

    update: prismic link resolver paths

--- config/site-config.js
+++ config/site-config.js
@@ -1,24 +1,37 @@
 const store = require("store");
 
 module.exports = {
-	isolatedViews: ["product", "lookbook"],
+	isolatedViews: ["product", "lookbook", "404"],
 	defaultLanguage: "en-us",
 	getLang: () => {
 		return store.get("lang") || "en-us";
 	},
+	setLang: (lang) => {
+		store.set("lang", lang || "en-us");
+	},
 	handleLinkResolver: (doc) => {
-		if (doc.type == "home") {
-			return "/";
-		} else if (doc.type == "lookbook") {
-			return "/lookbook";
-		} else if (doc.type == "shop") {
-			return "/shop";
-		} else if (doc.type == "product") {
-			return `/product/${doc.slug}`;
-		} else if (doc.type == "contact_us") {
-			return "/contact";
+		if (typeof doc === "string") {
+			return doc;
+		} else if (doc.type === "shop") {
+			return `/${doc.lang}/shop`;
+		} else if (doc.type === "lookbook") {
+			return `/${doc.lang}/lookbook`;
+		} else if (doc.type === "contact_us") {
+			return `/${doc.lang}/contact`;
+		} else if (doc.type === "product") {
+			return `/${doc.lang}/product/${doc.uid}`;
 		} else {
-			return `/${doc.slug}`;
+			return `/${doc.lang}/`;
+		}
+	},
+	handleLanguageDisplay: (lang) => {
+		switch (lang) {
+			case "en-us":
+				return "EN";
+			case "fr-ca":
+				return "FR";
+			default:
+				return "EN";
 		}
 	},
 };
